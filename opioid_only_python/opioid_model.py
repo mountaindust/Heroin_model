c18477fceb75e130ad1d7f8d26f3af3709288916
Christopher Strickland <cstric12@utk.edu>
2017-10-11 17:47:55 -0400

    Adds code to solve/plot opioid model

--- opioid_only_python/opioid_model.py
+++ opioid_only_python/opioid_model.py
@@ -39,11 +39,77 @@ def update_params(new_params):
 
 def opioid_odes(t, X, params):
     '''Specifies the opioid model as a system of ODEs.'''
-    pass
+    Y = np.empty((4)) # S,P,A,R
+    S = X[0]
+    P = X[1]
+    A = X[2]
+    R = X[3]
+    Y[0] = -params['alpha']*S-params['beta']*(1-params['xi'])*S*A-\
+        params['beta']*params['xi']*S*P+params['epsilon']*P+\
+        params['delta']*R+params['mu']*(P+R)+params['mu_star']*A
+    Y[1] = params['alpha']*S-(params['epsilon']+params['gamma']+params['mu'])*P
+    Y[2] = params['gamma']*P+params['sigma']*R+params['beta']*(1-params['xi'])*S*A+\
+        params['beta']*params['xi']*S*P+params['nu']*R*A-\
+        (params['zeta']+params['mu_star'])*A
+    Y[3] = params['zeta']*A-params['nu']*R*A-\
+        (params['delta']+params['sigma']+params['mu'])*R
+    return Y
 
 
 
-def solve_odes(S0,P0,A0,R0,tstart,tstop,params):
+def solve_odes(S0=S_0,P0=P_0,A0=A_0,R0=R_0,tstart=tstart,tstop=tstop,params=params):
     '''Solve opioid_odes with given initial conditions, time, and params.'''
-    pass
-    
+    S = [S0]
+    P = [P0]
+    A = [A0]
+    R = [R0]
+    # setup solver
+    solver = ode(opioid_odes).set_integrator('dopri5')
+    solver.set_initial_value([S0,P0,A0,R0], tstart).set_f_params(params)
+    # solve
+    while solver.successful() and solver.t < tstop:
+        solver.integrate(solver.t+1) #integrate up to next integer time
+        # record solution at that time
+        S.append(solver.y[0])
+        P.append(solver.y[1])
+        A.append(solver.y[2])
+        R.append(solver.y[3])
+    return (S,P,A,R)
+
+
+
+def compute_R0(params=params):
+    '''Check that AFE exists. If so, compute and return R0'''
+    if params['gamma'] != 0 or params['xi'] != 0:
+        raise ValueError('AFE does not exist with these parameters.')
+    else:
+        Lambda = 1 - params['sigma']/(params['delta']+params['mu']+params['sigma'])
+        S_star = 1 - params['alpha']/(params['alpha']+params['epsilon']+params['mu'])
+        return (params['beta']*S_star)/(params['mu']+params['zeta']*Lambda)
+
+
+
+def plot_solution(S,P,A,R,tstart=tstart,tstop=tstop,show=True):
+    '''Plot a solution set and either show it or return the plot object'''
+    t = np.arange(tstart,tstop+1,1)
+
+    fig = plt.figure(figsize=(8, 4.5))
+    plt.plot(t, S, label='Susceptible')
+    plt.plot(t, P, label="Prescription Users")
+    plt.plot(t, A, label="Addicts")
+    plt.plot(t, R, label="Recovering Addicts")
+    plt.legend()
+    plt.xlabel('Time (years)')
+    plt.ylabel('Population fraction')
+    plt.tight_layout()
+    if show:
+        plt.show()
+    else:
+        return fig
+
+if __name__ == "__main__":
+
+    #run model with default values and plot
+
+    sol = solve_odes()
+    plot_solution(*sol)
