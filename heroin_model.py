7ba2389fedafeb151eeaa95925e510c82ffe5bd3
Tricia Phillips <tphill30@vols.utk.edu>
2018-04-23 19:06:48 -0400

    reverting back to original parameters and implementing references in tex file

--- heroin_model.py
+++ heroin_model.py
@@ -16,11 +16,11 @@ tstop = 10000
 #parameters
 params = {}
 params['alpha'] = 0.2                       #S->P the rate at which people are prescribed opioids
-params['beta'] = 0.026                     #S->A total probability of becoming addicted to opioids other than by prescription 
+params['beta'] = 0.006                     #S->A total probability of becoming addicted to opioids other than by prescription
 params['xi'] = 0                           #MUST BE ZERO FOR AFE: S->A proportion of susceptibles that obtain extra prescription opioids OR black market drugs and becomes addicted 
 params['theta_1'] = 0.0102                  #S->H rate susceptible population becomes addicted to heroin by black market drugs and other addicts
 params['epsilon'] = 0.74                    #P->S rate at which people come back to the susceptible class after being prescribed opioids (i.e. not addicted)
-params['delta'] = 0.101                      #R->S rate at which people come back to the susceptible class after successfully finishing treatment 
+params['delta'] = 0.09                      #R->S rate at which people come back to the susceptible class after successfully finishing treatment
 params['mu'] = 0.00844                      #P,A,H,R->S natural death rate
 params['mu_A'] = 0.0000393                  #A->S enhanced death rate for opioid addicts ($\mu$ + overdose rate)
 params['mu_H'] = 0.0000430                  #H->S enhanced death rate for heroin addicts ($\mu$ + overdose rate) 
