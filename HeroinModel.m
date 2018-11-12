c375e720f08dd582a8383d5b2741b446a95144a5
Tricia Phillips <tphill30@vols.utk.edu>
2018-11-12 12:58:17 -0500

    put in another ODE Z=dy(7) to calculate number of new admissions throughout a year

--- HeroinModel.m
+++ HeroinModel.m
@@ -42,7 +42,10 @@ dy(3) = gamma*y(2)+sigma_A*y(5)+beta*(1-xi)*y(1)*y(3)+beta*xi*y(1)*y(2)-zeta*y(3
 dy(4) = theta_1*y(1)*y(4)+theta_2*y(2)*y(4)+theta_3*y(3)*y(4)+sigma_H*y(5)-nu*y(4)-(mu+mu_H)*y(4);
 dy(5) = zeta*y(3)+nu*y(4)-sigma_A*y(5)-sigma_H*y(5)-mu*y(5);
 %ODE for new cases of prescription opioid use over time 
-dy(6) = alpha*y(1);      
+dy(6) = alpha*y(1);
+%ODE for new admissions into recovery class over time from addicted and heroin classes  
+dy(7) = zeta*y(3)+nu*y(4);
+
   dy=dy';  
  
 end
