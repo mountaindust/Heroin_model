d0776c50bfd2195d9741be21f9f1b841c7acd58a
Tricia Phillips <tphill30@vols.utk.edu>
2018-11-12 12:59:58 -0500

    put in admissions equation to compare with data; changed notation to Estim1, Data1, diff1 and Estim2, Data2, diff2; updated value function

--- HeroinModel_ODE45.m
+++ HeroinModel_ODE45.m
@@ -49,7 +49,8 @@ A0=0.1;
 H0=.05;
 R0=.05; 
 X0=0;
-initials = [S0,P0,A0,H0,R0,X0];
+Z0=0;
+initials = [S0,P0,A0,H0,R0,X0,Z0];
 
 
 
@@ -62,6 +63,8 @@ initials = [S0,P0,A0,H0,R0,X0];
   H=y(:,4);
   R=y(:,5);
   X=y(:,6);
+  Z=y(:,7);
+  
   
  % FIRST WAY: In order to count the total number of individuals in P at some point throughout the year, 
  % we want to add the total number of individuals using prescriptions at the beginning of the
@@ -77,33 +80,53 @@ initials = [S0,P0,A0,H0,R0,X0];
 %Estim=[prescript(4),prescript(5),prescript(6),prescript(7),prescript(8)];
        
 
-%SECOND WAY: In order to count the total number of individuals in P at some point throughout the year, 
+ %SECOND WAY: In order to count the total number of individuals in P at some point throughout the year, 
  % we want to add the total number of individuals using prescriptions at the beginning of the
  % year to those who come into P at some point during the year: 
  % prescript(i) is number of prescription users at beginning of year(time step) +
- % the number of new cases that came in from y(6) ODE from that year until the beginning of the next year 
- % so adding in those who come into P at some point during the year so integrating y(6) ODE 
- % but just focusing in on the one year we care about 
+ % the number of new cases that came in from X'=dy(6) ODE from that year until the beginning of the next year 
+ % so adding in those who come into P at some point during the year by integrating y(6) ODE 
+ % but just focusing in on the one year we care about (so have to subtract)
  % Going to run from 2013 to 2017, so "IC" will be at 2013 and want info
  % for 2014-2017 so want:
+ 
  prescript=zeros(1,4);
  for i=1:4
  prescript(i) = y(i,2)+y(i+1,6)-y(i,6); 
  end
  
 %yearly output from the model as a fraction
- Estim=[prescript(1),prescript(2),prescript(3),prescript(4)];
+ Estim1=[prescript(1),prescript(2),prescript(3),prescript(4)];
        
 % Proportions of population of prescription opioid users (MADE UP FRACTIONS
 % FOR NOW)
- Data=[.1 .2 .3 .25];
+ Data1=[.1 .2 .3 .25];
  
 
 % the difference between estimated value and data 
- diff= Estim-Data;
+ diff1= Estim1-Data1;
+ 
+ 
+ 
+ 
+ %Using ODE y(7) in order to account for new admissions coming into recovery class
+ %(NOT total in recovery class), going to run from 2013-2015, so want:
+ admitted=zeros(1,2);
+ for i=1:2
+ admitted(i) = y(i+1,7)-y(i,7);
+ end
+ 
+ %yearly output from the model as a fraction
+ Estim2=[admitted(1), admitted(2)];
+  
+ %Proportions of population being admitted into recovery (MADE UP FRACTIONS FOR NOW)
+ Data2=[.001,.0015];
 
+ % the difference between estimated value and data 
+ diff2=Estim2-Data2;
+ 
  %the relative error that we are trying to minimize for ordinary least
  %squares: the sum of the squared errors (norm gives sum(diff.^2)^(1/2)) normalized by 
- value = norm(diff,2)./norm(Data); 
+ value = norm(diff1,2)./norm(Data1)+ norm(diff2,2)./norm(Data2);
  
  
