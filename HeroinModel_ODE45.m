70ef77bf954f7154fcd17184bbde849c5a844cf7
Tricia Phillips <tphill30@vols.utk.edu>
2018-11-12 12:44:11 -0500

    changing N to go from 2013-2017 only and changing prescript(i) indices

--- HeroinModel_ODE45.m
+++ HeroinModel_ODE45.m
@@ -1,10 +1,11 @@
 function value = HeroinModel_ODE45(z)
-%CORRECT?
 
 
-% Final time 
-N = 8;
+% Final time, note: don't want to run too long because dynamics
+% can drastically change over a number of years, so here we will do 2013-2017:
+N = 4; 
 T = N;
+
 %Generate N points, with spacing (T-0)/((N+1)-1)=1 between the points
 tspan=linspace(0,T,N+1);
 global value
@@ -53,7 +54,6 @@ initials = [S0,P0,A0,H0,R0,X0];
 
 
 [t,y]=ode45(@(t,y) HeroinModel(t,y,z),tspan,initials);
-%CORRECT?
 
 
   S=y(:,1);
@@ -77,19 +77,26 @@ initials = [S0,P0,A0,H0,R0,X0];
 %Estim=[prescript(4),prescript(5),prescript(6),prescript(7),prescript(8)];
        
 
-%SECOND WAY: Using ODE X'(t) in order to account for new cases coming into prescription
-%class
- prescript=zeros(4,8);
- for i=4:8
+%SECOND WAY: In order to count the total number of individuals in P at some point throughout the year, 
+ % we want to add the total number of individuals using prescriptions at the beginning of the
+ % year to those who come into P at some point during the year: 
+ % prescript(i) is number of prescription users at beginning of year(time step) +
+ % the number of new cases that came in from y(6) ODE from that year until the beginning of the next year 
+ % so adding in those who come into P at some point during the year so integrating y(6) ODE 
+ % but just focusing in on the one year we care about 
+ % Going to run from 2013 to 2017, so "IC" will be at 2013 and want info
+ % for 2014-2017 so want:
+ prescript=zeros(1,4);
+ for i=1:4
  prescript(i) = y(i,2)+y(i+1,6)-y(i,6); 
  end
  
 %yearly output from the model as a fraction
- Estim=[prescript(4),prescript(5),prescript(6),prescript(7),prescript(8)];
+ Estim=[prescript(1),prescript(2),prescript(3),prescript(4)];
        
 % Proportions of population of prescription opioid users (MADE UP FRACTIONS
 % FOR NOW)
- Data=[.1 .2 .3 .25 .4];
+ Data=[.1 .2 .3 .25];
  
 
 % the difference between estimated value and data 
