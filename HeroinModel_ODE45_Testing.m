cf68eed68282a3796ab31e69ef17ee1e80768e75
Tricia Phillips <tphill30@vols.utk.edu>
2018-12-01 14:31:02 -0500

    commenting out plots that were not correct to be compared to data points, updating estimations

--- HeroinModel_ODE45_Testing.m
+++ HeroinModel_ODE45_Testing.m
@@ -67,7 +67,10 @@ R0=z(16);
 X0=0;
 Z0=0;
 K0=0;
-initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
+L0=0;
+M0=0;
+initials = [S0,P0,A0,H0,R0,X0,Z0,K0,L0,M0];
+
 
 
 [t,y]=ode45(@(t,y) HeroinModel(t,y,z),tspan,initials);
@@ -81,6 +84,9 @@ initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
   X=y(:,6);
   Z=y(:,7);
   K=y(:,8);
+  L=y(:,9);
+  M=y(:,10);
+  
   
   
  %COMPARING MODEL ESTIMATES TO DATA 
@@ -104,7 +110,7 @@ initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
  end
  
 % We have total number of individuals who take prescription opioids for the
-% year 2013; so must take IC (which is estimated by z(13) because don't know number
+% year 2013; so must take IC (which is estimated because don't know number
 % at beginning of the year) and add on the number of individuals that enter
 % the P class at any point during the year 2013, which comes from
 % integrating ODE X'=dy(6) from t=0 to t=1; this gives first value in
@@ -112,13 +118,14 @@ initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
 
  
 % yearly output from the model as a proportion from 2013 to 2017
- Estim1=[z(13)+y(1,6), total_prescription_users(1),total_prescription_users(2),total_prescription_users(3), total_prescription_users(4)];
+ Estim1=[z(13)+y(1,6),total_prescription_users(1),total_prescription_users(2),total_prescription_users(3), total_prescription_users(4)];
        
 % actual proportions of population that were prescription opioid users for
 % 2013-2017 (total number of prescription opioid users in each year in TN that are 12 and older divided by
 % total population in TN 12 and older) 
  Data1=[1845144./5517176 1824342./5559006 1819581./5602117 1761363./5651993 1636374./5708586];
-
+%Data simulated when put in 0.1 for all parameters
+%Data1=[0.1 0.16 0.18 0.2 0.21];
 % the difference between estimated value and data 
  diff1= Estim1-Data1;
  
@@ -139,6 +146,8 @@ initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
   
  % actual proportions of population each year being admitted into recovery from opioid addict class
  Data2=[4485./5517176 4530./5559006 4326./5602117];
+ %%Data simulated when put in 0.1 for all parameters
+ %Data2=[0 0.011 0.013];
 
  % the difference between estimated value and data 
  diff2=Estim2-Data2;
@@ -162,26 +171,42 @@ initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
  % actual proportions of population each year being admitted into recovery from
  % heroin/fentanyl class
  Data3=[555./5517176 743./5559006 1083./5602117];
-
+ %%Data simulated when put in 0.1 for all parameters
+ %Data3=[0 0.01 0.011];
  % the difference between estimated value and data 
  diff3=Estim3-Data3;
  
 
  %%%%%
- % output from the model of the proportion of opioid addicts in 2015
+ % output from the model of the proportion of opioid addicts in 2015; we take
+ % initial number of opioid addicts in 2015, y(2,3), and add the number of individuals that enter
+% the A class at any point during the year 2015, which comes from
+% integrating ODE L'=dy(9) but just focusing in on the one year, 2015, we care about 
+ %(so have to subtract: integrating gives total number of new cases from t=0 to t=3, so have to 
+ % subtract off the number from t=0 to t=2). 
  % MAY NEED TO FIX VALUE IF DECIDE THIS NUMBER COUNTS INDIVIDUALS IN RECOVERY, TOO 
- Estim4=[y(2,3)];
+ Estim4=[y(2,3)+y(3,9)-y(2,9)];
  % made up for now
  Data4=[48000./5602117];
+ %Data simulated when put in 0.1 for all parameters
+ %Data4=[0.12];
  % the difference between estimated value and data
  diff4=Estim4-Data4;
  
 
  %%%%%
- % output from the model of the proportion of heroin/fentanyl addicts in 2015
- Estim5=[y(2,4)];
+ % output from the model of the proportion of heroin/fentanyl addicts in
+ % 2015; we take initial number of heroin/fentanyl addicts in 2015, y(2,4),
+ % and add the number of individuals that enter
+ % the H class at any point during the year 2015, which comes from
+ % integrating ODE M'=dy(10) but just focusing in on the one year, 2015, we care about 
+ %(so have to subtract: integrating gives total number of new cases from t=0 to t=3, so have to 
+ % subtract off the number from t=0 to t=2). 
+ Estim5=[y(2,4)+y(3,10)-y(2,10)];
  % Made up for now
  Data5=[14000./5602117];
+ %Data simulated when put in 0.1 for all parameters
+ %Data5=[0.11];
  % the difference between estimated value and data
  diff5=Estim5-Data5;
  
@@ -192,14 +217,15 @@ initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
  %normalized by norm of the data
  value = norm(diff1,2)./norm(Data1)+ norm(diff2,2)./norm(Data2)+...
  norm(diff3,2)./norm(Data3)+norm(diff4,2)./norm(Data4)+norm(diff5,2)./norm(Data5);
- 
+
+
  
  figure(1)
  hold all
  scatter(1:1:5, Estim1,'filled')
  scatter(1:1:5, Data1, 'filled')
- plot(t,y(:,2),'b-','LineWidth',1)
- set(gca, 'xtick', [0 1 2 3 4])
+ %plot(t,y(:,2),'b-','LineWidth',1)
+ set(gca, 'xtick', [1 2 3 4 5])
  set(gca, 'fontsize',10)
  set(gca,'xticklabel',{'2013','2014','2015','2016','2017'})
  xlabel('Year')
@@ -235,7 +261,7 @@ initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
  hold all
  scatter(1:1:1, Estim4,'filled')
  scatter(1:1:1, Data4, 'filled')
- plot(t,y(:,3),'b-','LineWidth',1)
+ %plot(t,y(:,3),'b-','LineWidth',1)
  set(gca, 'xtick', [ 1 ])
  set(gca, 'fontsize',10)
  set(gca,'xticklabel',{'2015'})
@@ -248,7 +274,7 @@ initials = [S0,P0,A0,H0,R0,X0,Z0,K0];
  hold all
  scatter(1:1:1, Estim5,'filled')
  scatter(1:1:1, Data5, 'filled')
- plot(t,y(:,4) ,' b-','LineWidth',1)
+ %plot(t,y(:,4) ,' b-','LineWidth',1)
  set(gca, 'xtick', [ 1 ])
  set(gca, 'fontsize',10)
  set(gca,'xticklabel',{'2015'})
