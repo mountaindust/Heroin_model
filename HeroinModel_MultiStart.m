3b34251ae7d25685d3220bfdc7f669fd8d2ee5cb
Tricia Phillips <tphill30@vols.utk.edu>
2018-12-06 19:02:09 -0500

    putting in more realistic lower and upper bounds of parameters

--- HeroinModel_MultiStart.m
+++ HeroinModel_MultiStart.m
@@ -3,15 +3,22 @@
 clear all; 
 clc;
 
-% the parameter vector we will approximate
-% z=[alpha, beta_A, beta_P, theta_1, epsilon, gamma, theta_2,
-% sigma_A, zeta, theta_3, sigma_H, nu, P0, A0, H0, R0]
 
 
-% The vector of the initilized parameters 
-LowerBounds=[0.00001  0.00001  0.00001  0.00001 0.00001  0.00001 0.00001 0.00001 0.00001  0.00001  0.00001  0.00001 0.00001 0.00001 0.00001 0.00001];
-%z0=[0.5 0.3 0.8 0.000008 0.00027 0.0000003 0.0000006 0.0000001 0.000001 0.2815 0.1];
-UpperBounds=[0.99999  0.99999  0.99999  0.99999 0.99999  0.99999 0.99999 0.99999 0.99999  0.99999  0.99999  0.99999 0.99999 0.99999 0.99999 0.99999];
+% the parameter vector we will approximate 
+%        z =[alpha  beta_A    beta_P   theta_1   epsilon  gamma   theta_2   sigma_A    zeta    theta_3  sigma_H    nu       P0      A0        H0       R0  ]
+LowerBounds=[0.01    0.00001  0.00001   0.00001    0.8   0.00001  0.00002   0.00001   0.00001  0.00004  0.00001  0.00001   0.01   0.00001   0.00001  0.00001];
+UpperBounds=[0.7      0.1      0.1       0.1        8      0.1      0.2        2       2        0.4        2        2      0.8      0.2       0.1      0.3];
+
+% For now: alpha guess from opioid paper since higher in TN; beta_A guess from opioid paper; 
+% beta_P guess from opioid paper; theta_1 complete guess, assume smaller than beta_A, beta_P; 
+% epsilon 0.8-8 from opioid paper; gamma guess from opioid paper; theta_2
+% guess twice as large as theta_1; sigma_A guess small because
+% "successful recovereds"; zeta will be smaller than opioid paper because less "relapse" but still
+% put opioid largest value in; theta_3 guess four times as large as
+% theta_1; sigma_H guessed same as sigma_A; nu guess same as zeta; P0
+% guess; A0 guess; H0 guess; R0 guess 
+
 
 xstart=0.5*(LowerBounds + UpperBounds); % initial guesses for parameters (starting in the middle of each of the ranges)
 
@@ -23,7 +30,7 @@ problem=createOptimProblem('fmincon','x0', xstart,'objective',@HeroinModel_ODE45
 problem.options=optimoptions(problem.options, 'MaxFunEvals',99999,'MaxIter',99999);
 
 %number of times want to do 
-numstartpoints=400;
+numstartpoints=4;
 
 ms=MultiStart('Display', 'iter'); % Define a multistart problem
 
