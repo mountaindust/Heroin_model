5b66284f7bf99f11e73d21d2378584a8bad2a734
Tricia Phillips <tphill30@vols.utk.edu>
2019-03-20 00:01:22 -0400

    updating to check newest R0 calculation with finalized model

--- heroin_R0/CheckingR0.py
+++ heroin_R0/CheckingR0.py
@@ -5,23 +5,21 @@ import matplotlib.pyplot as plt
 from scipy.integrate import ode
 
 params = {}
-params['alpha'] = 0.207                       #S->P the rate at which people are prescribed opioids #[20] https://www.cdc.gov/drugoverdose/pdf/pubs/2017-cdc-drug-surveillance-report.pdf (page 39, table 1A)
-params['beta_A'] = 0.006                     #S->A total probability of becoming addicted to opioids other than by prescription #assume okay for now until find updated/source
-params['beta_P'] =  0                     # S->A proportion of susceptibles that obtain extra prescription opioids OR black market drugs and becomes addicted (Note: MUST BE ZERO FOR AFE) #assume okay for now until find updated/source
+params['alpha'] = 0.2                       #S->P the rate at which people are prescribed opioids #[20] https://www.cdc.gov/drugoverdose/pdf/pubs/2017-cdc-drug-surveillance-report.pdf (page 39, table 1A)
+params['beta_A'] = 0.000273                    #S->A total probability of becoming addicted to opioids other than by prescription #assume okay for now until find updated/source
+params['beta_P'] =  0                    # S->A proportion of susceptibles that obtain extra prescription opioids OR black market drugs and becomes addicted (Note: MUST BE ZERO FOR AFE) #assume okay for now until find updated/source
 params['theta_1'] = 0.0003                  #S->H rate susceptible population becomes addicted to heroin by black market drugs and other addicts #ESTIMATED [30] https://www.drugabuse.gov/publications/drugfacts/heroin#ref
-params['delta'] = 0.15                     #R->S rate at which people come back to the susceptible class after successfully finishing treatment #[19] https://ac.els-cdn.com/S0740547213000779/1-s2.0-S0740547213000779-main.pdf?_tid=0a47e661-2ac3-45fb-a7da-9fa8c43271af&acdnat=1525189437_134c2f5932fa797a0725faa7c950a0f9 (page 1, 10% successfully treated for opioids + ESTIMATED 5% successfully treated for heroin)
-params['mu'] = 0.00844                      #P,A,H,R->S natural death rate  # [24] https://www.cdc.gov/nchs/data/nvsr/nvsr66/nvsr66_05.pdf (page 1)
-params['mu_A'] = 0.00004                  #A->S enhanced death rate for opioid addicts (only overdose rate=4/100,000) # [25] https://www.cdc.gov/drugoverdose/data/analysis.html (Opioid Data Analysis, figure)
-params['mu_H'] = 0.00004                 #H->S enhanced death rate for heroin addicts (only overdose rate=4/100,000) # [25] https://www.cdc.gov/drugoverdose/data/analysis.html  (Opioid Data Analysis, figure)
+params['mu'] = 0.00868                      #P,A,H,R->S natural death rate  # [24] https://www.cdc.gov/nchs/data/nvsr/nvsr66/nvsr66_05.pdf (page 1)
+params['mu_A'] = 0.00775                  #A->S enhanced death rate for opioid addicts (only overdose rate=4/100,000) # [25] https://www.cdc.gov/drugoverdose/data/analysis.html (Opioid Data Analysis, figure)
+params['mu_H'] = 0.0271                 #H->S enhanced death rate for heroin addicts (only overdose rate=4/100,000) # [25] https://www.cdc.gov/drugoverdose/data/analysis.html  (Opioid Data Analysis, figure)
 params['gamma'] = 0          # P->A rate at which prescribed opioid users become addicted (Note: MUST BE ZERO FOR AFE) # [28] https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1360-0443.2010.03052.x
-params['epsilon'] = 1-params['gamma']         #P->S rate at which people come back to the susceptible class after being prescribed opioids (i.e. not addicted) #[28] https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1360-0443.2010.03052.x (1-0.26=0.74)
-params['theta_2'] = 0.0003                       #P->H rate at which opioid prescribed user population becomes addicted to heroin #[30] https://www.drugabuse.gov/publications/drugfacts/heroin#ref
-params['sigma_A'] = 0.9  #R->A rate at which people relapse from treatment into the opioid addicted class #[19] https://ac.els-cdn.com/S0740547213000779/1-s2.0-S0740547213000779-main.pdf?_tid=0a47e661-2ac3-45fb-a7da-9fa8c43271af&acdnat=1525189437_134c2f5932fa797a0725faa7c950a0f9 (page 1, 90% relapse rate by end of one year)
+params['epsilon'] = 1.5         #P->S rate at which people come back to the susceptible class after being prescribed opioids (i.e. not addicted) #[28] https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1360-0443.2010.03052.x (1-0.26=0.74)
+params['theta_2'] = 0.0009                       #P->H rate at which opioid prescribed user population becomes addicted to heroin #[30] https://www.drugabuse.gov/publications/drugfacts/heroin#ref
+params['sigma'] = 0.7  #R->A rate at which people relapse from treatment into the opioid addicted class #[19] https://ac.els-cdn.com/S0740547213000779/1-s2.0-S0740547213000779-main.pdf?_tid=0a47e661-2ac3-45fb-a7da-9fa8c43271af&acdnat=1525189437_134c2f5932fa797a0725faa7c950a0f9 (page 1, 90% relapse rate by end of one year)
 params['zeta'] = 0.25                        #A->R rate at which addicted opioid users enter treatment/rehabilitation #assume okay for now until find updated/source
-params['theta_3'] = 0.04                    #A->H rate at which the opioid addicted population becomes addicted to heroin #[14] https://d14rmgtrwzf5a.cloudfront.net/sites/default/files/19774-prescription-opioids-and-heroin.pdf (page 7)
-params['sigma_H'] = 0.95    #R->H rate at which people relapse from treatment back into the heroin addicted class #ESTIMATED for now because cannot find source yet
-params['nu'] = .15                          #H->R rate at which heroin users enter treatment/rehabilitation #[14] https://d14rmgtrwzf5a.cloudfront.net/sites/default/files/19774-prescription-opioids-and-heroin.pdf (page 17)
-
+params['theta_3'] = 0.0048                   #A->H rate at which the opioid addicted population becomes addicted to heroin #[14] https://d14rmgtrwzf5a.cloudfront.net/sites/default/files/19774-prescription-opioids-and-heroin.pdf (page 7)
+params['nu'] = .1                          #H->R rate at which heroin users enter treatment/rehabilitation #[14] https://d14rmgtrwzf5a.cloudfront.net/sites/default/files/19774-prescription-opioids-and-heroin.pdf (page 17)
+params['omega']=0.0000000001
 
 
 def update_params(new_params):
@@ -34,42 +32,29 @@ def update_params(new_params):
             print('Could not find parameter {}.'.format(key))
 
          
-F = np.matrix( ((params['beta_A']*(params['epsilon']+params['mu'])/(params['alpha']+params['epsilon']+params['mu']),0, 0),
+F = np.matrix( ((params['beta_A']*(params['epsilon']+params['mu'])/(params['alpha']+params['epsilon']+params['mu']),0),
                  (0, params['theta_1']*(params['epsilon']+params['mu'])/(params['alpha']+params['epsilon']+params['mu'])\
-                 +params['theta_2']*params['alpha']/(params['alpha']+params['epsilon']+params['mu']),0), 
-                 (0, 0, 0)) ) 
+                 +params['theta_2']*params['alpha']/(params['alpha']+params['epsilon']+params['mu']))) ) 
 
-V = np.matrix( ((params['zeta']+params['mu']+params['mu_A'],0,-params['sigma_A']), 
-                (0, params['nu']+params['mu']+params['mu_H'], -params['sigma_H']),
-                (-params['zeta'],-params['nu'],params['delta']+params['sigma_A']+params['sigma_H']+params['mu'])) )
+V = np.matrix( ((params['zeta']+params['mu']+params['mu_A'],0), 
+                (0, params['nu']+params['mu']+params['mu_H'])) )
 
 Vinv = inv(np.matrix(V))
 
 M = np.matmul(F, Vinv)
 
-#print(LA.eigvals(M)) #R0 is the spectral radus of F*Vinv; compare to value computed below
-# print(det(V))
+print(LA.eigvals(M)) #R0 is the spectral radus of F*Vinv; compare to value computed below
+#print(det(V))
 
 def compute_R0(p=None):
       '''Check that AFE exists. If so, compute and return R0'''
       if p is None:
          p = params
-      if p['gamma'] != 0 or p['beta_P'] != 0: #i.e. if gamma not =0 or xi not =0, then AFE DNE. Need at least one to be 0. 
+      if p['gamma'] != 0 or p['beta_P'] != 0: #i.e. if gamma not =0 or beta_P not =0, then AFE DNE. Need at least one to be 0. 
          raise ValueError('AFE does not exist with these parameters.')
       else:
-         S_star = (params['epsilon']+params['mu'])/(params['alpha']+params['epsilon']+params['mu'])
-         P_star = params['alpha']/(params['alpha']+params['epsilon']+params['mu'])
-         a= params['zeta']+params['mu']+params['mu_A']
-         b= params['nu']+params['mu']+params['mu_H']
-         c= params['delta']+params['sigma_A']+params['sigma_H']+params['mu']
-         z= params['theta_1']*S_star+params['theta_2']*P_star   
-         detV= a*(b*c-params['sigma_H']*params['nu'])-params['sigma_A']*params['zeta']*b
-         print((1/detV)*(1/2)*\
-               (params['beta_A']*S_star*(b*c-params['sigma_H']*params['nu'])+z*(a*c-params['zeta']*params['sigma_A'])+\
-               ((params['beta_A']*S_star*(b*c-params['sigma_H']*params['nu'])+z*(a*c-params['zeta']*params['sigma_A']))**2-\
-               4*(params['beta_A']*S_star*z*(b*c-params['sigma_H']*params['nu'])*(a*c-params['zeta']*params['sigma_A'])-\
-               params['beta_A']*S_star*z*params['sigma_A']*params['nu']\
-               *params['sigma_H']*params['zeta']))**.5))
+         print(params['beta_A']*(params['epsilon']+params['mu'])/((params['alpha']+params['epsilon']+params['mu'])*(params['zeta']+params['mu']+params['mu_A'])),
+         (params['theta_1']*(params['epsilon']+params['mu'])+params['theta_2']*params['alpha'])/((params['alpha']+params['epsilon']+params['mu'])*(params['nu']+params['mu']+params['mu_H'])))
 
 # #do "return" without extra ( ) if don't want to print when run 
 #in order to see this value printed in ipython: do "import CheckingR0 as cr" and then "cr.compute_R0()" or if just
