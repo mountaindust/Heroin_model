3bb967e0ef0ee814ffc97edfa7348d14aed5df67
Tricia Phillips <tphill30@vols.utk.edu>
2019-03-27 13:36:57 -0400

    updating zeta and nu values that were estimated

--- heroin_R0/CheckingR0.py
+++ heroin_R0/CheckingR0.py
@@ -6,9 +6,9 @@ from scipy.integrate import ode
 
 params = {}
 params['alpha'] = 0.2                       #S->P the rate at which people are prescribed opioids #[20] https://www.cdc.gov/drugoverdose/pdf/pubs/2017-cdc-drug-surveillance-report.pdf (page 39, table 1A)
-params['beta_A'] = 0.000273                    #S->A total probability of becoming addicted to opioids other than by prescription #assume okay for now until find updated/source
+params['beta_A'] = .4#0.000273                    #S->A total probability of becoming addicted to opioids other than by prescription #assume okay for now until find updated/source
 params['beta_P'] =  0                    # S->A proportion of susceptibles that obtain extra prescription opioids OR black market drugs and becomes addicted (Note: MUST BE ZERO FOR AFE) #assume okay for now until find updated/source
-params['theta_1'] = 0.0003                  #S->H rate susceptible population becomes addicted to heroin by black market drugs and other addicts #ESTIMATED [30] https://www.drugabuse.gov/publications/drugfacts/heroin#ref
+params['theta_1'] = .4#0.0003                  #S->H rate susceptible population becomes addicted to heroin by black market drugs and other addicts #ESTIMATED [30] https://www.drugabuse.gov/publications/drugfacts/heroin#ref
 params['mu'] = 0.00868                      #P,A,H,R->S natural death rate  # [24] https://www.cdc.gov/nchs/data/nvsr/nvsr66/nvsr66_05.pdf (page 1)
 params['mu_A'] = 0.00775                  #A->S enhanced death rate for opioid addicts (only overdose rate=4/100,000) # [25] https://www.cdc.gov/drugoverdose/data/analysis.html (Opioid Data Analysis, figure)
 params['mu_H'] = 0.0271                 #H->S enhanced death rate for heroin addicts (only overdose rate=4/100,000) # [25] https://www.cdc.gov/drugoverdose/data/analysis.html  (Opioid Data Analysis, figure)
@@ -16,9 +16,9 @@ params['gamma'] = 0          # P->A rate at which prescribed opioid users become
 params['epsilon'] = 1.5         #P->S rate at which people come back to the susceptible class after being prescribed opioids (i.e. not addicted) #[28] https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1360-0443.2010.03052.x (1-0.26=0.74)
 params['theta_2'] = 0.0009                       #P->H rate at which opioid prescribed user population becomes addicted to heroin #[30] https://www.drugabuse.gov/publications/drugfacts/heroin#ref
 params['sigma'] = 0.7  #R->A rate at which people relapse from treatment into the opioid addicted class #[19] https://ac.els-cdn.com/S0740547213000779/1-s2.0-S0740547213000779-main.pdf?_tid=0a47e661-2ac3-45fb-a7da-9fa8c43271af&acdnat=1525189437_134c2f5932fa797a0725faa7c950a0f9 (page 1, 90% relapse rate by end of one year)
-params['zeta'] = 0.25                        #A->R rate at which addicted opioid users enter treatment/rehabilitation #assume okay for now until find updated/source
+params['zeta'] = 0.0214                        #A->R rate at which addicted opioid users enter treatment/rehabilitation #assume okay for now until find updated/source
 params['theta_3'] = 0.0048                   #A->H rate at which the opioid addicted population becomes addicted to heroin #[14] https://d14rmgtrwzf5a.cloudfront.net/sites/default/files/19774-prescription-opioids-and-heroin.pdf (page 7)
-params['nu'] = .1                          #H->R rate at which heroin users enter treatment/rehabilitation #[14] https://d14rmgtrwzf5a.cloudfront.net/sites/default/files/19774-prescription-opioids-and-heroin.pdf (page 17)
+params['nu'] = .0155                         #H->R rate at which heroin users enter treatment/rehabilitation #[14] https://d14rmgtrwzf5a.cloudfront.net/sites/default/files/19774-prescription-opioids-and-heroin.pdf (page 17)
 params['omega']=0.0000000001
 
 
