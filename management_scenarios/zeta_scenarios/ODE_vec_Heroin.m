3cb31fa3c0b57a3c4f1281164b6986432b8247fd
Tricia Phillips <tphill30@vols.utk.edu>
2019-11-04 18:29:03 -0500

    effect on A,H of changing zeta

---
+++ management_scenarios/zeta_scenarios/ODE_vec_Heroin.m
@@ -0,0 +1,90 @@
+function dydt=LV_ODE_vec_Heroin(t,y,vec_matrix,x)
+%% PARAMETERS %%
+Parameter_settings_vec_Heroin;
+%global LHSmatrix
+
+m= vec_matrix(x,1);
+beta_A= vec_matrix(x,2);
+beta_P= vec_matrix(x,3);
+theta_1= vec_matrix(x,4);
+epsilon= vec_matrix(x,5);
+gamma= vec_matrix(x,6);
+sigma= vec_matrix(x,7);
+mu= vec_matrix(x,8);
+mu_H= vec_matrix(x,9);
+theta_2= vec_matrix(x,10);
+zeta= vec_matrix(x,11);
+theta_3= vec_matrix(x,12);
+nu= vec_matrix(x,13);
+omega= vec_matrix(x,14);
+b= vec_matrix(x,15);
+c=vec_matrix(x,16);
+
+P0=0.0949727450989279;
+A0=0.00709742287302280;
+H0=0.000464895055434927;
+R0=0.00507229016950725;
+S0=1-P0-A0-H0-R0;
+     
+%% Distemper model for shelter
+%have included a 5th equation that keeps track of the total exposed, which
+%will be the metric we are interested in
+
+%  dydt = [b - beta_s*y(1)*y(3) - delta*y(1) - a_s*y(1)
+%         beta_s*y(1)*y(3) + beta_v*y(4)*y(3) - a_e*y(2) - alpha*y(2)
+%         alpha*y(2) - d*y(3)
+%         delta*y(1) - beta_v*y(4)*y(3) - a_v*y(4)
+%         beta_s*y(1)*y(3) + beta_v*y(4)*y(3)];
+
+    
+    
+   dydt=[-a(t,m,b,c)*y(1)-beta_A*y(1)*y(3)-beta_P*y(1)*y(2)-theta_1*y(1)*y(4)+epsilon*y(2)+mu*(y(2)+y(5))+(mu+muA(t,d,e))*y(3)+(mu+mu_H)*y(4)
+         a(t,m,b,c)*y(1)-epsilon*y(2)-gamma*y(2)-theta_2*y(2)*y(4)-mu*y(2)
+         gamma*y(2)+sigma*y(5)*y(3)/(y(3)+y(4)+omega)+beta_A*y(1)*y(3)+beta_P*y(1)*y(2)-zeta*y(3)-theta_3*y(3)*y(4)-(mu+muA(t,d,e))*y(3)
+         theta_1*y(1)*y(4)+theta_2*y(2)*y(4)+theta_3*y(3)*y(4)+sigma*y(5)*y(4)/(y(3)+y(4)+omega)-nu*y(4)-(mu+mu_H)*y(4)
+         zeta*y(3)+nu*y(4)-sigma*y(5)*y(3)/(y(3)+y(4)+omega)-sigma*y(5)*y(4)/(y(3)+y(4)+omega)-mu*y(5)];
+    
+     
+end
+
+
+function alpha = a(t,m,b,c)
+if  t<=3.25 
+    alpha = m*t+b;
+else 
+    alpha = m*3.25+b-c*3.25+c*t;
+end
+end
+
+
+function mu_A = muA(t,d,e)
+    mu_A = d*t+e;
+end
+
+
+%LEAVING OUT %% FOR NOW from Christina's Code 
+
+%%function val = gamma(epi1b,epi2b,hospitalizations)
+
+% function for controlling the rate at which people seek medical attention
+%In theory, as more incidents are noticed, media/health officials get
+%involved and start informing the general public which in turn makes people
+%more aware of an outbreak and makes them more likely to seek medical
+%attention.
+
+%I beleive this is a function of detected infections (ie, hospiutalized)
+%rather than a function of dead indiviudals.
+% maxval = 1;
+% minval = .25;
+%exponential
+%val = .75*exp(-epi*hospitalizations);
+%first sigmoidal try
+%val = maxval/(1+((maxval-minval)/minval)*exp(-epi*hospitalizations));
+%second sigmoidal try
+%%val =(1-exp(-hospitalizations/epi1b))/(1+epi2b*exp(-hospitalizations/epi1b));
+
+%%val = max(val,0);
+
+%%end
+    
+   
---
+++ management_scenarios/zeta_scenarios/zeta_uniformly_distributed.m
@@ -0,0 +1,83 @@
+
+Parameter_settings_vec_Heroin;
+
+%Number of intervals to uniformly sample zeta 
+N=100;
+
+%Create vectors of parameters (with zeta varying) to run model with
+m_vec=linspace(m,m,N);
+beta_A_vec=linspace(beta_A,beta_A,N);
+beta_P_vec=linspace(beta_P,beta_P,N);
+theta_1_vec=linspace(theta_1,theta_1,N);
+epsilon_vec=linspace(epsilon, epsilon,N);
+gamma_vec=linspace(gamma,gamma,N);
+sigma_vec=linspace(sigma,sigma,N);
+mu_vec=linspace(mu,mu,N);
+mu_H_vec=linspace(mu_H,mu_H,N);
+theta_2_vec=linspace(theta_2,theta_2,N);
+zeta_vec=linspace(0.1*zeta,zeta,N);
+theta_3_vec=linspace(theta_3,theta_3,N);
+nu_vec=linspace(nu,nu,N);
+omega_vec=linspace(omega,omega,N);
+b_vec=linspace(b,b,N);
+c_vec=linspace(c,c,N);
+d_vec=linspace(d,d,N);
+e_vec=linspace(e,e,N);
+
+%Matrix of parameter sets
+%vec_matrix  = [m_vec,beta_A_vec,beta_P_vec,theta_1_vec,epsilon_vec,gamma_vec,sigma_vec,mu_vec,mu_A_vec,mu_H_vec,theta_2_vec,zeta_vec,theta_3_vec,nu_vec,omega_vec,b_vec,c_vec];
+vec_matrix  = [m_vec;beta_A_vec;beta_P_vec;theta_1_vec;epsilon_vec;gamma_vec;sigma_vec;mu_vec;mu_H_vec;theta_2_vec;zeta_vec;theta_3_vec;nu_vec;omega_vec;b_vec;c_vec;d_vec;e_vec]';
+
+
+%Run ODE with rows of vec_matrix 
+for x=1:N;
+    f=@ODE_vec_Heroin;
+
+
+    [t,y] = ode15s(@(t,y)f(t,y,vec_matrix,x),tspan,y0,[]); 
+
+    W = [t y];
+    
+    S_lhs(:,x)=W(time_points+1,2);
+    P_lhs(:,x)=W(time_points+1,3);
+    A_lhs(:,x)=W(time_points+1,4);
+    H_lhs(:,x)=W(time_points+1,5);
+    R_lhs(:,x)=W(time_points+1,6);
+    
+    
+end  
+    
+ %Percent change in zeta: taking baseline zeta and subtracting new zeta
+ %value and dividing by baseline. Then plotting final time output of A from baseline
+ %zeta-final time output of A from new zeta value divided by baseline final time output. Same
+ %for H. 
+ figure(1);
+ hold all
+ plot((0.198182427387906-vec_matrix(:,11))*100/0.198182427387906,(0.004329295236935-A_lhs(1,:))*100/0.004329295236935,'LineWidth',2) 
+ hold on
+ plot((0.198182427387906-vec_matrix(:,11))*100/0.198182427387906,(0.004298941991162-H_lhs(1,:))*100/0.004298941991162,'LineWidth',2)
+ xlabel('Percent reduction in \zeta')
+ ylabel('Percent reduction in A or H')
+ %legend({'Percent reduction in A', 'Percent reduction in H'},'FontSize', 16)
+ set(gca,'XTick',0:10:100);
+ set(gca,'FontSize',16)
+ xlim([0 60])
+ ylim([0 100])
+ 
+ figure(2);
+ plot(vec_matrix(:,11),A_lhs(1,:),'LineWidth',2) 
+ %set ( gca, 'xdir', 'reverse' )
+ xlabel('\zeta')
+ ylabel('A')
+ set(gca,'FontSize',16)
+ xlim([0 0.2])
+
+ figure(3);
+ plot(vec_matrix(:,11),H_lhs(1,:),'-r','LineWidth',2)
+ %set ( gca, 'xdir', 'reverse' )
+ xlabel('\zeta')
+ ylabel('H')
+ set(gca,'FontSize',16)
+ xlim([0 0.2])
+ 
+ 
