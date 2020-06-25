768d91d30ee5ac5f12201e56e1cb65fe3d20f83d
Owen Queen <owencqueen@hotmail.com>
2020-06-25 12:48:57 -0400

    Updated functions

--- ABM/data_analysis/individual_functions/helpers.R
+++ ABM/data_analysis/individual_functions/helpers.R
@@ -17,3 +17,187 @@ find_range = function(upper, lower, range_pct){
   if (d < 0){ d = 0 } # Reset lower to 0 if needed
   return (c(u, d))
 }
+
+
+# meanLineABM:
+#   Returns a vector corresponding to the mean line for ABM data
+
+# Arguments:
+#   1. abm = two options:
+#       1) character value corresponding to filename
+#       2) dataframe
+
+#   2. plot_class = character value corresponding to class of interest
+#       - Options: 'S', 'N', 'P', 'P_S', 'P_N', 'A', 'H', 'R'
+
+#   3. need_name = boolean corresponding to whether dataframe needs to be renamed
+#       - If dataframe needs to be renamed: TRUE
+#       - If dataframe does not need renaming: FALSE
+
+meanLineABM = function(abm, plot_class, need_name = TRUE){
+  
+  # Get runs data
+  if (is.character(abm)){
+    abm = read.csv(as.character(abm))
+  }
+  
+  if (need_name){
+    abm = abm %>% rename(run = "X.run.number.", step = "X.step.")
+  }
+  
+  # Get steps and runs for each
+  steps = max(abm$step)
+  runs  = max(abm$run)
+  
+  # Option for class
+  
+  if(plot_class == "S"){
+    if (need_name){
+      abm = abm %>% rename(S = "count.turtles.with..class....S.....turtle.count")
+    }
+    
+    interest = abm$S
+  }
+  if(plot_class == "N"){
+    if (need_name){
+      abm = abm %>% rename(N = "count.turtles.with..class....N.....turtle.count")
+    }
+    
+    interest = abm$N
+  }
+  if(plot_class == "P"){
+    if(need_name){
+      abm = abm %>% rename(P = "count.turtles.with..class....P.....turtle.count")
+    }
+    
+    interest = abm$P
+  }
+  if(plot_class == "P_S"){
+    if (need_name){
+      abm = abm %>% rename(P_S = "count.turtles.with..class....P_S.....turtle.count")
+    }
+    
+    interest = abm$P_S
+  }
+  if(plot_class == "P_N"){
+    if (need_name){
+      abm = abm %>% rename(P_N = "count.turtles.with..class....P_N.....turtle.count")
+    }
+    
+    interest = abm$P_N
+  }
+  if(plot_class == "A"){
+    if (need_name){
+      abm = abm %>% rename(A = "count.turtles.with..class....A.....turtle.count")
+    }
+    
+    interest = abm$A
+  }
+  if(plot_class == "H"){
+    if (need_name){
+      abm = abm %>% rename(H = "count.turtles.with..class....H.....turtle.count")
+    }
+    
+    interest = abm$H
+  }
+  if(plot_class == "R"){
+    if (need_name){
+      abm = abm %>% rename(R = "count.turtles.with..class....R.....turtle.count")
+    }
+    
+    interest = abm$R
+  }
+  
+  mval = c()
+  for (i in seq(1, as.integer(steps), by = 1)){  
+    mval = c(mval, mean(interest[abm$step == i]))
+  }
+  
+  return(mval)
+}
+
+
+# ABM_df: generates a csv file name for ABM data based off of the name of the file
+
+# Arguments:
+#   1. abm: name of file to be retrieved, converted
+#   2. N: specifies whether abm is SPAHR or SNPAHR
+#     - If abm is SPAHR data, N = FALSE
+#     - If abm is SNPAHR data, N = TRUE
+
+ABM_df = function(abm){
+  
+  if (!(is.character(abm))){ # If abm is already a dataframe, don't need conversion
+    return(abm)
+  }
+  
+  abm_data = read.csv(as.character(abm))
+  
+  abm_data = abm_data %>% rename(run = "X.run.number.", step = "X.step.")
+  
+  abm_data = abm_data %>% rename(S = "count.turtles.with..class....S.....turtle.count")
+  abm_data = abm_data %>% rename(A = "count.turtles.with..class....A.....turtle.count")
+  abm_data = abm_data %>% rename(H = "count.turtles.with..class....H.....turtle.count")
+  abm_data = abm_data %>% rename(R = "count.turtles.with..class....R.....turtle.count")
+  
+  N = FALSE
+  if("count.turtles.with..class....N.....turtle.count" %in% colnames(abm)){
+    N = TRUE
+  }
+  
+  if (N){
+    abm_data = abm_data %>% rename(N = "count.turtles.with..class....N.....turtle.count")
+    abm_data = abm_data %>% rename(P_S = "count.turtles.with..class....P_S.....turtle.count")
+    abm_data = abm_data %>% rename(P_N = "count.turtles.with..class....P_N.....turtle.count")
+  } else {
+    abm_data = abm_data %>% rename(P = "count.turtles.with..class....P.....turtle.count")
+  }
+  
+  return(abm_data)
+}
+
+
+# abm_data MUST BE A DATAFRAME already read in
+
+# downsample_ABM: downsamples ABM steps by specified factor
+
+# Arguments:
+#   1. adm_data: dataframe on which to downsample
+#   2. every: specifies the factor which you want to downsample by
+#     - i.e. your final df will have (nrow(abm_data) / every) rows
+#     - Gets steps that are multiples of this number
+#   3. verbose: option to print steps to screen during runtime
+#
+
+downsample_ABM = function(abm_data, every = 10, verbose = TRUE){
+  
+  runs = max(abm_data$run)
+  steps = max(abm_data$step)
+  
+  ret_abm = data.frame()
+  
+  for (j in seq(0, runs)){
+    
+    # Access all of the necessary steps within each run
+    for (i in seq(from = 0, to = steps, by = every)){
+    
+      if ((i == 0) && (j == 0)){ # Initialize new dataframe
+        
+        ret_abm = data.frame(subset(abm_data, step == i & run == j))
+        colnames(ret_abm) = colnames(abm_data)                       # Make sure we rename the df
+        
+      } else {
+        ret_abm = rbind(ret_abm, subset(abm_data, step == i & run == j))
+      }
+      
+      if (verbose){
+        print(paste( paste("run: ", as.character(j)), paste("--- step: ", as.character(i)))) # print progress
+      }
+    }
+  }
+
+  # Convert values in step col
+  ret_abm$step = ret_abm$step / every
+  
+  return (ret_abm)
+}
--- ABM/data_analysis/individual_functions/plot_SPAHR.R
+++ ABM/data_analysis/individual_functions/plot_SPAHR.R
@@ -4,22 +4,36 @@
 
 #  Arguments:
 #     1. abm: name of abm data
+#       - Two options:
+#         1) abm is the name of a file
+#           - If so, this file is converted and retrieved
+#         2) abm is a pre-made dataframe that assumes ABM standard naming
 #     2. ode: name of ode data
+#       - Two options:
+#         1) ode is the name of a file 
+#           - If so, this file is converted and retrieved
+#         2) ode is a pre-made dataframe
 #     3. plot_class: name of class to plot (default "S")
 #       - current options: "S","P","A","H","R"
+#     4. N: specifies whether you are plotting the SPAHR or SNPAHR model
+#       - if SPAHR, N = FALSE
+#       - if SNPAHR, N = TRUE
 
-plotSPAHR = function(abm, ode, plot_class = "S", N = FALSE){
-  
-  abm_data = read.csv(as.character(abm))
-  ode_data = read.csv(as.character(ode))
-  
-  abm_data = abm_data %>% rename(run = "X.run.number.", step = "X.step.")
 
+plotSPAHR = function(abm, ode, plot_class = "S"){
+  
+  # If "adm" is a name of a file, we need to retrieve that file
+  abm_data = ABM_df(abm)
+  
+  # If "ode" is a name of a file, retrieve the file
+  if (is.character(ode)){
+    ode_data = read.csv(as.character(ode))
+  }
+  
   steps = max(abm_data$step)
   runs  = max(abm_data$run)
   
   if(plot_class == "S"){
-    abm_data = abm_data %>% rename(S = "count.turtles.with..class....S.....turtle.count")
     plt_c   = abm_data$S
     plt_ode = ode_data$S
     
@@ -28,7 +42,6 @@ plotSPAHR = function(abm, ode, plot_class = "S", N = FALSE){
     clr       = "steelblue"
   }
   if(plot_class == "N"){
-    abm_data = abm_data %>% rename(N = "count.turtles.with..class....N.....turtle.count")
     plt_c   = abm_data$N
     plt_ode = ode_data$N
     
@@ -36,32 +49,27 @@ plotSPAHR = function(abm, ode, plot_class = "S", N = FALSE){
     clr       = "turquoise"
   }
   if(plot_class == "P"){
-    abm_data = abm_data %>% rename(P = "count.turtles.with..class....P.....turtle.count")
     plt_c   = abm_data$P
     plt_ode = ode_data$P
     
     plot_head = "P Proportions"
-    clr       = "yellow2"
+    clr       = "yellow3"
   }
   if(plot_class == "P_S"){
-    abm_data = abm_data %>% rename(P_S = "count.turtles.with..class....P_S.....turtle.count")
     plt_c    = abm_data$P_S
-    #plt_ode = <what>
+    plt_ode  = ode_data$PS
     
     plot_head = "P_S Proportions"
-    clr       = "yellow2"
-    
+    clr       = "yellow3"
   }
   if(plot_class == "P_N"){
-    abm_data = abm_data %>% rename(P_N = "count.turtles.with..class....P_N.....turtle.count")
     plt_c    = abm_data$P_N
-    #plt_ode = <what>
+    plt_ode  = ode_data$PN
     
     plot_head = "P_N Proportoins"
     clr       = "orange"
   }
   if(plot_class == "A"){
-    abm_data = abm_data %>% rename(A = "count.turtles.with..class....A.....turtle.count")
     plt_c   = abm_data$A
     plt_ode = ode_data$A 
     
@@ -69,7 +77,6 @@ plotSPAHR = function(abm, ode, plot_class = "S", N = FALSE){
     clr       = "tomato"
   }
   if(plot_class == "H"){
-    abm_data = abm_data %>% rename(H = "count.turtles.with..class....H.....turtle.count")
     plt_c    = abm_data$H
     plt_ode = ode_data$H 
     
@@ -77,7 +84,6 @@ plotSPAHR = function(abm, ode, plot_class = "S", N = FALSE){
     clr       = "violet"
   }
   if(plot_class == "R"){
-    abm_data = abm_data %>% rename(R = "count.turtles.with..class....R.....turtle.count")
     plt_c   = abm_data$R
     plt_ode = ode_data$R
     
@@ -85,6 +91,7 @@ plotSPAHR = function(abm, ode, plot_class = "S", N = FALSE){
     clr       = "palegreen3"
   }
 
+  
   # Find range we need for plotting:
   
   mx = max(plt_c)
@@ -109,6 +116,7 @@ plotSPAHR = function(abm, ode, plot_class = "S", N = FALSE){
     }
   }
   
+  # Plot the mean line on the same graph
   mval = plt_c[abm_data$step == 0 & abm_data$run == 1]
   for (i in seq(1, as.integer(steps), by = 1)){  
     mval = c(mval, mean(plt_c[abm_data$step == i]))
@@ -120,24 +128,4 @@ plotSPAHR = function(abm, ode, plot_class = "S", N = FALSE){
   #  legend(0, 0.07, title = "Legend",legend = c("ABM", "ABM Mean", "ODE"), col = c("orange", "black", "blue"), lty = c(1,1,1),ncol = 1)
   legend("topright", cex = 0.8, title = "Legend",legend = c("ABM", "ABM Mean", "ODE"), col = c(clr, "black", "blue"), lty = c(1,1,1),ncol = 1)
   
-  
-}
-
-# find_range: **********************
-# Gets the range of values to include for the plots
-#     Arguments:
-#       1. upper     : maximum value of the data being considered
-#       2. lower     : minimum value of the data
-#       3. range_pct : percent of the range to go up/down for range
-find_range = function(upper, lower, range_pct){
-  
-  ran = upper - lower # Range to consider
-  
-  ran_add = ran * range_pct # Add to max/min
-  
-  u = upper + ran_add # Max and min vals
-  d = lower - ran_add
-  
-  if (d < 0){ d = 0 } # Reset lower to 0 if needed
-  return (c(u, d))
 }
